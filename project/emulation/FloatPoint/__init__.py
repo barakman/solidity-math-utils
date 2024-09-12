74a93d7b2f81a7d1a6a0385dc21c48bee971fb94
barak manos <>
2024-09-12 11:59:51 +0300

    Cosmetic

--- project/emulation/FloatPoint/__init__.py
+++ project/emulation/FloatPoint/__init__.py
@@ -9,64 +9,64 @@ getcontext().rounding = ROUND_HALF_DOWN
 
 def pow(a, b, c, d, factor):
     a, b, c, d, factor = [Decimal(value) for value in vars().values()]
-    return (a/b)**(c/d)*factor
+    return (a / b) ** (c / d) * factor
 
 
 def log(a, b, factor):
     a, b, factor = [Decimal(value) for value in vars().values()]
-    return (a/b).ln()*factor
+    return (a / b).ln() * factor
 
 
 def exp(a, b, factor):
     a, b, factor = [Decimal(value) for value in vars().values()]
-    return (a/b).exp()*factor
+    return (a / b).exp() * factor
 
 
 def lambertNeg(x, factor):
     x, factor = [Decimal(value) for value in vars().values()]
-    return lambertRatio(-x/factor)*factor
+    return lambertRatio(-x / factor) * factor
 
 
 def lambertPos(x, factor):
     x, factor = [Decimal(value) for value in vars().values()]
-    return lambertRatio(+x/factor)*factor
+    return lambertRatio(+x / factor) * factor
 
 
 def buy(supply, balance, weight, amount):
     supply, balance, weight, amount = [Decimal(value) for value in vars().values()]
-    return supply*((1+amount/balance)**(weight/MAX_WEIGHT)-1)
+    return supply * ((1 + amount / balance) ** (weight / MAX_WEIGHT) - 1)
 
 
 def sell(supply, balance, weight, amount):
     supply, balance, weight, amount = [Decimal(value) for value in vars().values()]
-    return balance*(1-(1-amount/supply)**(MAX_WEIGHT/weight))
+    return balance * (1 - (1 - amount / supply) ** (MAX_WEIGHT / weight))
 
 
 def convert(balance1, weight1, balance2, weight2, amount):
     balance1, weight1, balance2, weight2, amount = [Decimal(value) for value in vars().values()]
-    return balance2*(1-(balance1/(balance1+amount))**(weight1/weight2))
+    return balance2 * (1 - (balance1 / (balance1 + amount)) ** (weight1 / weight2))
 
 
 def deposit(supply, balance, weights, amount):
     supply, balance, weights, amount = [Decimal(value) for value in vars().values()]
-    return supply*((amount/balance+1)**(weights/MAX_WEIGHT)-1)
+    return supply * ((amount / balance + 1) ** (weights / MAX_WEIGHT) - 1)
 
 
 def withdraw(supply, balance, weights, amount):
     supply, balance, weights, amount = [Decimal(value) for value in vars().values()]
-    return balance*(1-((supply-amount)/supply)**(MAX_WEIGHT/weights))
+    return balance * (1 - ((supply - amount) / supply) ** (MAX_WEIGHT / weights))
 
 
 def invest(supply, balance, weights, amount):
     supply, balance, weights, amount = [Decimal(value) for value in vars().values()]
-    return balance*(((supply+amount)/supply)**(MAX_WEIGHT/weights)-1)
+    return balance * (((supply + amount) / supply) ** (MAX_WEIGHT / weights) - 1)
 
 
 def lambertRatio(x):
     a = x if x < 1 else x.ln()
     for _ in range(8):
         e = a.exp()
-        f = a * e
+        f = a  *  e
         if f == x: break
-        a = (a * f + x) / (f + e)
-    return a / x
+        a = (a  *  f  +  x)  /  (f  +  e)
+    return a  /  x
