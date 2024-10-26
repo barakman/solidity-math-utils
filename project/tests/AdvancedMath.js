db69b9508369d81fa144bc33cdc59231fb776a77
barak manos <>
2024-10-26 13:43:42 +0300

    Improve test

--- project/tests/AdvancedMath.js
+++ project/tests/AdvancedMath.js
@@ -11,7 +11,7 @@ const SECTIONS = [
     Decimal(Constants.LAMBERT_CONV_RADIUS),
     Decimal(Constants.LAMBERT_POS2_MAXVAL),
     Decimal(Constants.LAMBERT_POS3_MAXVAL),
-    Decimal(Constants.LAMBERT_POS2_MAXVAL).mul(100)
+    Decimal(Constants.LAMBERT_POS3_MAXVAL).mul(48)
 ];
 
 function solvable(a, b, c, d) {
@@ -51,11 +51,11 @@ describe(TestContract.contractName, () => {
 
     for (let percent = 0; percent <= 100; percent++) {
         testSuccess("lambertNeg", percent, -1, 0, 1, "0.13251");
-        testSuccess("lambertPos", percent, +1, 0, 4, "0.06272");
+        testSuccess("lambertPos", percent, +1, 0, 4, "0.06143");
         testSuccess("lambertPos", percent, +1, 0, 1, "0.00353");
         testSuccess("lambertPos", percent, +1, 1, 2, "0.00006");
         testSuccess("lambertPos", percent, +1, 2, 3, "0.06484");
-        testSuccess("lambertPos", percent, +1, 2, 4, "0.06230");
+        testSuccess("lambertPos", percent, +1, 2, 4, "0.06166");
     }
 
     testFailure("lambertNeg", 0, 0, "lambertNeg: x < min");
