867e2f1d6ae108a08b85581b0796434355ca2065
barak manos <>
2024-10-26 16:01:08 +0300

    Improve emulation tests

--- project/emulation/TestAdvancedMathLambertPos4.py
+++ project/emulation/TestAdvancedMathLambertPos4.py
@@ -12,19 +12,27 @@ MAX_VAL = FixedPoint.AdvancedMath.LAMBERT_POS3_MAXVAL * 100 + 1
 def test(x):
     fixedPoint = FixedPoint.lambertPos(x)
     floatPoint = FloatPoint.lambertPos(x, FIXED_1)
-    return fixedPoint / floatPoint
+    if floatPoint > fixedPoint:
+        error = ['Implementation Error:']
+        error.append(f'x          = {x         }')
+        error.append(f'fixedPoint = {fixedPoint}')
+        error.append(f'floatPoint = {floatPoint}')
+        raise Exception('\n'.join(error))
+    return floatPoint / fixedPoint
 
 
 size = int(sys.argv[1] if len(sys.argv) > 1 else input('How many test-cases would you like to execute? '))
 
 
-minRatio = float('+inf')
-maxRatio = float('-inf')
+worstAccuracy = 1
 
 
 for n in range(size):
     x = random.randrange(MIN_VAL, MAX_VAL)
-    ratio = test(x)
-    minRatio = min(minRatio, ratio)
-    maxRatio = max(maxRatio, ratio)
-    print(f'Test #{n}: ratio = {ratio:.24f}, minRatio = {minRatio:.24f}, maxRatio = {maxRatio:.24f}')
+    try:
+        accuracy = test(x)
+        worstAccuracy = min(worstAccuracy, accuracy)
+    except Exception as error:
+        print(error)
+        break
+    print(f'Test #{n}: accuracy = {accuracy:.24f}, worstAccuracy = {worstAccuracy:.24f}')
