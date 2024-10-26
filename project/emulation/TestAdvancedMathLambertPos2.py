244c8ee7662116bc4de991bea965b05c825b11f3
barak manos <>
2024-10-26 16:26:47 +0300

    Revert previous changes in some of the emulation tests

--- project/emulation/TestAdvancedMathLambertPos2.py
+++ project/emulation/TestAdvancedMathLambertPos2.py
@@ -12,27 +12,19 @@ MAX_VAL = FixedPoint.AdvancedMath.LAMBERT_POS2_MAXVAL + 1
 def test(x):
     fixedPoint = FixedPoint.lambertPos(x)
     floatPoint = FloatPoint.lambertPos(x, FIXED_1)
-    if fixedPoint < floatPoint:
-        error = ['Implementation Error:']
-        error.append(f'x          = {x         }')
-        error.append(f'fixedPoint = {fixedPoint}')
-        error.append(f'floatPoint = {floatPoint}')
-        raise Exception('\n'.join(error))
     return fixedPoint / floatPoint
 
 
 size = int(sys.argv[1] if len(sys.argv) > 1 else input('How many test-cases would you like to execute? '))
 
 
-worstAccuracy = 1
+minRatio = float('+inf')
+maxRatio = float('-inf')
 
 
 for n in range(size):
     x = random.randrange(MIN_VAL, MAX_VAL)
-    try:
-        accuracy = test(x)
-        worstAccuracy = max(worstAccuracy, accuracy)
-    except Exception as error:
-        print(error)
-        break
-    print(f'Test #{n}: accuracy = {accuracy:.24f}, worstAccuracy = {worstAccuracy:.24f}')
+    ratio = test(x)
+    minRatio = min(minRatio, ratio)
+    maxRatio = max(maxRatio, ratio)
+    print(f'Test #{n}: ratio = {ratio:.24f}, minRatio = {minRatio:.24f}, maxRatio = {maxRatio:.24f}')
