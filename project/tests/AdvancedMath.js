2c03bd7ced170bc0e244858424b36e4690fab968
barak manos <>
2024-10-30 07:45:42 +0200

    Optimize the extent of lambert-pos2 from 3 to 24

--- project/tests/AdvancedMath.js
+++ project/tests/AdvancedMath.js
@@ -10,7 +10,7 @@ const SECTIONS = [
     Decimal(0),
     Decimal(Constants.LAMBERT_CONV_RADIUS),
     Decimal(Constants.LAMBERT_POS2_MAXVAL),
-    Decimal(Constants.LAMBERT_POS2_MAXVAL).mul(100)
+    Decimal(Constants.LAMBERT_POS2_MAXVAL).mul(10)
 ];
 
 function solvable(a, b, c, d) {
@@ -40,7 +40,7 @@ describe(TestContract.contractName, () => {
         for (const b of [1, 2, 3, 4, 5])
             for (const c of [1, 2, 3, 4, 5])
                 for (const d of [1, 2, 3, 4, 5])
-                    testSolve(a, b, c, d, "0.09154");
+                    testSolve(a, b, c, d, "0.00883");
 
     for (const a of [1, 2, 3, 4, 5].map(n => n + 1000))
         for (const b of [1, 2, 3, 4, 5].map(n => n + 1000))
@@ -50,10 +50,10 @@ describe(TestContract.contractName, () => {
 
     for (let percent = 0; percent <= 100; percent++) {
         testSuccess("lambertNeg", percent, -1, 0, 1, "0.13251");
-        testSuccess("lambertPos", percent, +1, 0, 3, "0.01409");
+        testSuccess("lambertPos", percent, +1, 0, 3, "0.00273");
         testSuccess("lambertPos", percent, +1, 0, 1, "0.00353");
-        testSuccess("lambertPos", percent, +1, 1, 2, "0.00006");
-        testSuccess("lambertPos", percent, +1, 2, 3, "0.04400");
+        testSuccess("lambertPos", percent, +1, 1, 2, "0.00203");
+        testSuccess("lambertPos", percent, +1, 2, 3, "0.00273");
     }
 
     testFailure("lambertNeg", 0, 0, "lambertNeg: x < min");
