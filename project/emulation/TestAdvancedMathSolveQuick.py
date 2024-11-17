c50b3f68c8ce89215536fdbb0ebf1bb8b7a36f26
barak manos <>
2024-11-17 10:26:57 +0200

    Improve emulation tests

--- project/emulation/TestAdvancedMathSolveQuick.py
+++ project/emulation/TestAdvancedMathSolveQuick.py
@@ -1,33 +1,21 @@
-import sys
 import random
 import FixedPoint
 import FloatPoint
+import TestScheme
 
 
-def test(a, b, c, d):
-    p, q = FixedPoint.solveQuick(a, b, c, d)
-    a, b, c, d, p, q = [FloatPoint.Decimal(value) for value in [a, b, c, d, p, q]]
-    return (p / q) * (a / b) ** (p / q) / (c / d)
-
-
-size = int(sys.argv[1] if len(sys.argv) > 1 else input('How many test-cases would you like to execute? '))
-
-
-minRatio = float('+inf')
-maxRatio = float('-inf')
-numOfFailures = 0
-
-
-for n in range(size):
+def getInput():
     a = random.randrange(1, 1000)
     b = random.randrange(1, 1000)
     c = random.randrange(1, 1000)
     d = random.randrange(1, 1000)
-    try:
-        ratio = test(a, b, c, d)
-        minRatio = min(minRatio, ratio)
-        maxRatio = max(maxRatio, ratio)
-    except AssertionError as error:
-        ratio = 0
-        numOfFailures += 1
-    print(f'Test #{n}: ratio = {ratio:.24f}, minRatio = {minRatio:.24f}, maxRatio = {maxRatio:.24f}, numOfFailures = {numOfFailures}')
+    return dict(a=a, b=b, c=c, d=d)
+
+
+def getOutput(a, b, c, d):
+    p, q = FixedPoint.solveQuick(a, b, c, d)
+    a, b, c, d, p, q = [FloatPoint.Decimal(value) for value in [a, b, c, d, p, q]]
+    return (p / q) * (a / b) ** (p / q), (c / d), True
+
+
+TestScheme.run(getInput, getOutput)
--- project/emulation/TestDynamicCurveEqualizeQuick.py
+++ project/emulation/TestDynamicCurveEqualizeQuick.py
@@ -1,41 +1,26 @@
-import sys
 import random
 import FixedPoint
 import FloatPoint
+import TestScheme
 
 
-Decimal = FloatPoint.Decimal
-
-
-def test(staked1, balance1, balance2, rate1, rate2):
-    weights = FixedPoint.equalizeQuick(staked1, balance1, balance2, rate1, rate2)
-    weight1 = weights[0]
-    weight2 = weights[1]
-    amount1 = staked1 - balance1
-    amount2 = FloatPoint.convert(balance1, weight1, balance2, weight2, amount1)
-    return Decimal((balance1 + amount1) * rate1 * weight2) / Decimal((balance2 - amount2) * rate2 * weight1)
-
-
-size = int(sys.argv[1] if len(sys.argv) > 1 else input('How many test-cases would you like to execute? '))
-
-
-minRatio = float('+inf')
-maxRatio = float('-inf')
-numOfFailures = 0
-
-
-for n in range(size):
+def getInput():
     rate     = random.randrange(10 ** 18, 10 ** 21)
     staked1  = random.randrange(10 ** 24, 10 ** 27)
     balance1 = staked1 * random.randrange(75, 150) // 100
     balance2 = staked1 * random.randrange(75, 150) // 100
     rate1    = rate * random.randrange(75, 150) // 100
     rate2    = rate * random.randrange(75, 150) // 100
-    try:
-        ratio = test(staked1, balance1, balance2, rate1, rate2)
-        minRatio = min(minRatio, ratio)
-        maxRatio = max(maxRatio, ratio)
-    except AssertionError as error:
-        ratio = 0
-        numOfFailures += 1
-    print(f'Test #{n}: ratio = {ratio:.24f}, minRatio = {minRatio:.24f}, maxRatio = {maxRatio:.24f}, numOfFailures = {numOfFailures}')
+    return dict(staked1=staked1, balance1=balance1, balance2=balance2, rate1=rate1, rate2=rate2)
+
+
+def getOutput(staked1, balance1, balance2, rate1, rate2):
+    weights = FixedPoint.equalizeQuick(staked1, balance1, balance2, rate1, rate2)
+    weight1 = weights[0]
+    weight2 = weights[1]
+    amount1 = staked1 - balance1
+    amount2 = FloatPoint.convert(balance1, weight1, balance2, weight2, amount1)
+    return (balance1 + amount1) * rate1 * weight2, (balance2 - amount2) * rate2 * weight1, True
+
+
+TestScheme.run(getInput, getOutput)
