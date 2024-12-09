7dff92260088499e357a2246ae96ca2e27598e5d
barak manos <>
2024-12-09 05:05:07 +0200

    Improve emulation testing infrastructure

--- project/emulation/FloatPoint/__init__.py
+++ project/emulation/FloatPoint/__init__.py
@@ -7,58 +7,58 @@ getcontext().prec = 100
 getcontext().rounding = ROUND_HALF_DOWN
 
 
-def pow(a, b, c, d, factor):
-    a, b, c, d, factor = [Decimal(value) for value in vars().values()]
+def pow(*args):
+    a, b, c, d, factor = [Decimal(arg) for arg in args]
     return (a / b) ** (c / d) * factor
 
 
-def log(a, b, factor):
-    a, b, factor = [Decimal(value) for value in vars().values()]
+def log(*args):
+    a, b, factor = [Decimal(arg) for arg in args]
     return (a / b).ln() * factor
 
 
-def exp(a, b, factor):
-    a, b, factor = [Decimal(value) for value in vars().values()]
+def exp(*args):
+    a, b, factor = [Decimal(arg) for arg in args]
     return (a / b).exp() * factor
 
 
-def lambert(x, factor):
-    x, factor = [Decimal(value) for value in vars().values()]
+def lambert(*args):
+    x, factor = [Decimal(arg) for arg in args]
     return lambertRatio(x / factor) * factor
 
 
-def solve(a, b, c, d, x, y):
-    a, b, c, d, x, y = [Decimal(value) for value in vars().values()]
+def solve(*args):
+    a, b, c, d, x, y = [Decimal(arg) for arg in args]
     return (x / y) * (a / b) ** (x / y) / (c / d)
 
 
-def buy(supply, balance, weight, amount, max_weight):
-    supply, balance, weight, amount, max_weight = [Decimal(value) for value in vars().values()]
+def buy(*args):
+    supply, balance, weight, amount, max_weight = [Decimal(arg) for arg in args]
     return supply * ((1 + amount / balance) ** (weight / max_weight) - 1)
 
 
-def sell(supply, balance, weight, amount, max_weight):
-    supply, balance, weight, amount, max_weight = [Decimal(value) for value in vars().values()]
+def sell(*args):
+    supply, balance, weight, amount, max_weight = [Decimal(arg) for arg in args]
     return balance * (1 - (1 - amount / supply) ** (max_weight / weight))
 
 
-def convert(balance1, weight1, balance2, weight2, amount):
-    balance1, weight1, balance2, weight2, amount = [Decimal(value) for value in vars().values()]
+def convert(*args):
+    balance1, weight1, balance2, weight2, amount = [Decimal(arg) for arg in args]
     return balance2 * (1 - (balance1 / (balance1 + amount)) ** (weight1 / weight2))
 
 
-def deposit(supply, balance, weights, amount, max_weight):
-    supply, balance, weights, amount, max_weight = [Decimal(value) for value in vars().values()]
+def deposit(*args):
+    supply, balance, weights, amount, max_weight = [Decimal(arg) for arg in args]
     return supply * ((amount / balance + 1) ** (weights / max_weight) - 1)
 
 
-def withdraw(supply, balance, weights, amount, max_weight):
-    supply, balance, weights, amount, max_weight = [Decimal(value) for value in vars().values()]
+def withdraw(*args):
+    supply, balance, weights, amount, max_weight = [Decimal(arg) for arg in args]
     return balance * (1 - ((supply - amount) / supply) ** (max_weight / weights))
 
 
-def invest(supply, balance, weights, amount, max_weight):
-    supply, balance, weights, amount, max_weight = [Decimal(value) for value in vars().values()]
+def invest(*args):
+    supply, balance, weights, amount, max_weight = [Decimal(arg) for arg in args]
     return balance * (((supply + amount) / supply) ** (max_weight / weights) - 1)
 
 
