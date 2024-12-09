3f2854b5a98e83b763fa9fed111182a0dd99c546
barak manos <>
2024-12-09 05:10:50 +0200

    Improve emulation testing infrastructure

--- project/emulation/FloatPoint/__init__.py
+++ project/emulation/FloatPoint/__init__.py
@@ -7,58 +7,61 @@ getcontext().prec = 100
 getcontext().rounding = ROUND_HALF_DOWN
 
 
+parse = lambda args: [Decimal(arg) for arg in args]
+
+
 def pow(*args):
-    a, b, c, d, factor = [Decimal(arg) for arg in args]
+    a, b, c, d, factor = parse(args)
     return (a / b) ** (c / d) * factor
 
 
 def log(*args):
-    a, b, factor = [Decimal(arg) for arg in args]
+    a, b, factor = parse(args)
     return (a / b).ln() * factor
 
 
 def exp(*args):
-    a, b, factor = [Decimal(arg) for arg in args]
+    a, b, factor = parse(args)
     return (a / b).exp() * factor
 
 
 def lambert(*args):
-    x, factor = [Decimal(arg) for arg in args]
+    x, factor = parse(args)
     return lambertRatio(x / factor) * factor
 
 
 def solve(*args):
-    a, b, c, d, x, y = [Decimal(arg) for arg in args]
+    a, b, c, d, x, y = parse(args)
     return (x / y) * (a / b) ** (x / y) / (c / d)
 
 
 def buy(*args):
-    supply, balance, weight, amount, max_weight = [Decimal(arg) for arg in args]
+    supply, balance, weight, amount, max_weight = parse(args)
     return supply * ((1 + amount / balance) ** (weight / max_weight) - 1)
 
 
 def sell(*args):
-    supply, balance, weight, amount, max_weight = [Decimal(arg) for arg in args]
+    supply, balance, weight, amount, max_weight = parse(args)
     return balance * (1 - (1 - amount / supply) ** (max_weight / weight))
 
 
 def convert(*args):
-    balance1, weight1, balance2, weight2, amount = [Decimal(arg) for arg in args]
+    balance1, weight1, balance2, weight2, amount = parse(args)
     return balance2 * (1 - (balance1 / (balance1 + amount)) ** (weight1 / weight2))
 
 
 def deposit(*args):
-    supply, balance, weights, amount, max_weight = [Decimal(arg) for arg in args]
+    supply, balance, weights, amount, max_weight = parse(args)
     return supply * ((amount / balance + 1) ** (weights / max_weight) - 1)
 
 
 def withdraw(*args):
-    supply, balance, weights, amount, max_weight = [Decimal(arg) for arg in args]
+    supply, balance, weights, amount, max_weight = parse(args)
     return balance * (1 - ((supply - amount) / supply) ** (max_weight / weights))
 
 
 def invest(*args):
-    supply, balance, weights, amount, max_weight = [Decimal(arg) for arg in args]
+    supply, balance, weights, amount, max_weight = parse(args)
     return balance * (((supply + amount) / supply) ** (max_weight / weights) - 1)
 
 
